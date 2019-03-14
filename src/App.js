d783cbe28c545b0e1773956405beaa11c0859150
austinjblake <46485261+austinjblake@users.noreply.github.com>
2019-03-13 20:58:01 -0500

    added face recognition with Clarifai API and drew boundaries

--- src/App.js
+++ src/App.js
@@ -22,15 +22,34 @@ const particlesOptions = {
       }
     }
   }
-}
+} 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
   }
 
   onInputChange = (e) => {
@@ -42,14 +61,8 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-      .then(
-    function(response) {
-      console.log(response);
-    },
-    function(err) {
-      // there was an error
-    }
-  );
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(error => console.log(error));
   }
 
   render() {
@@ -65,7 +78,7 @@ class App extends Component {
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
