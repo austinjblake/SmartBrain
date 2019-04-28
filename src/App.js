df1f915fa51f3364624bd77a4ec965e120d64e96
austinjblake <46485261+austinjblake@users.noreply.github.com>
2019-04-28 17:33:17 -0500

    deploying to heroku

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://enigmatic-anchorage-13624.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -97,7 +97,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://enigmatic-anchorage-13624.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -109,8 +109,8 @@ class App extends Component {
             this.setState(Object.assign(this.state.user, { entries: count}))
           })
           .catch(console.log)
-      }
       this.displayFaceBox(this.calculateFaceLocation(response))
+      }
     })
     .catch(error => console.log(error));
   }
